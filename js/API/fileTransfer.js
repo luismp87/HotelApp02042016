cec53ab44ef08bf54cf5ad6f11f7d486fbb20059
luismp87 <luismp87@gmail.com>
2016-04-16 09:45:31 -0500

    16 04 2016

--- js/API/fileTransfer.js
+++ js/API/fileTransfer.js
@@ -1,6 +1,6 @@
 //fileTransfer.js
 var ft = {
-	obj: new FileTransfer(),
+	//obj: new FileTransfer(),
 	win: function (r) {
 		if(r.response==1){
 			window.localStorage.setItem("user",$('#regNom').val());
@@ -16,6 +16,7 @@ var ft = {
 		options.fileName = "orlando";
 		options.mimeType = "image/jpeg";
 		
-		ft.obj.upload(fileURL, "http://carlos.igitsoft.com/apps/test.php", ft.win, ft.fail, options);
+        var ft2 = new FIleTransfer();
+		ft2.upload(fileURL, "http://carlos.igitsoft.com/apps/test.php", ft.win, ft.fail, options);
 	}
 };
