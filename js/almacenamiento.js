97cbea4c40a170331de90ef273a8a72ab2bab311
luismp87 <luismp87@gmail.com>
2016-04-23 10:51:04 -0500

    ss

--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -1,38 +1,48 @@
 var almacen = {
- pr: null,
- di: null,
- th: null,
- guardarReserva : function(pr,di,th){
-  almacen.pr = pr;
-  almacen.di = di;
-  almacen.th = th;
-  
-  almacen.db = window.openDatabase("hotelApp","1.0","HotelApp Storage",20000);
-  almacen.db.transaction(almacen.hacerReserva,almacen.error,almacen.reservaGuardada);
- },
- hacerReserva: function(tx){
-  tx.executeSql("CREATE TABLE IF NOT EXISTS reservas (pr,di,th)");
-  tx.executeSql("INSERT INTO reservas (pr,di,th) VALUES ('"+ almacen.pr + "','" + almacen.di + "','" + almacen.th + "')");
- },
- error: function(){
-  alert("Error al acceder a la Base de Datos");
- },
- reservaGuardada : function(){
-  navigator.notification.alert("Reserva guardada en espera de sincronización",almacen.leerReservas, "Felicidades", "Aceptar");
- },
- leerReservas: function(){
-  almacen.db.transaction(almacen.consultaReservas,almacen.error,null);
-  
- },
- consultaReservas: function(tx){
-  tx.executeSql("SELECT * FROM reservas", [], function(tx2, t){
-       for(i = 0; i < t.rows.length; i++){
-        navigator.notification.confirm("Personas: " + t.rows.item(i).pr + "\n" + "Dias: " + t.rows.item(i).di + "\n" + "Tipo de Habitación: " + t.rows.item(i).th,
-              function(btn){
-         if(btn == 1) navigator.vibrate(1000);
-         if(btn == 2) navigator.notification.beep(1);
-        },"Tabla Reservas","Vibrar,Sonar,Cancelar");
-       }
-       });
- }
+    pr: null,
+    di: null,
+    th: null,
+    
+    guardarReserva: function(pr,di,th){
+        almacen.pr = pr;
+        alamcen.di = di;
+        alamcen.th = th;
+        almacen.db = window.openDatabase("hotelApp","1.0","HotelApp Storage",20000);
+        almacen.db.transaction(almacen.hacerReserva,almacen.error, almacen.reservaGuardada);
+        
+    },
+    hacerReserva: function(tx)
+    {
+        tx.executeSql("CREATE TABLE IF NOT EXISTS reservas (pr,di,th)");
+        tx.executeSql("INSERT INTO reservas (pr,di,th) VALUES ('"+almacen.pr+"','"+almacen.di+"','"+almacen.th+"')");         
+    },
+    error: function()
+    {
+        alert("Error al acceder a la base de datos");  
+    },
+    reservaGuardada: function()
+    {        
+        navigator.notification.alert("Reserva guardada en espera de sincronizacion", almacen.leerReservas, "Felicidades","Aceptar");
+    },
+    leerReservas: function()
+    {
+        almacen.db.trasaction(almacen.consultaReserva, almacen.error,null);
+    },
+    consultaReservas: function(tx)
+    {
+        tx.executeSql("SELECT * FROM reservas",[],function(tx2,tabla)
+        {
+            for(i =0;i<tabla.rows.length; i++)
+                {
+                    navigator.notification.confirm("Personas: " + tabla.rows.item(i).pr + "\n" 
+                                                   + "Dias: " + tabla.rows.item(i).di + "\n"
+                                                  + "Tipo de habitacion: " + tabla.rows.item(i).th,
+                                                  function(btn)
+                                                   {
+                                                        if(btn == 1)  navigator.vibrate(500);
+                                                        if(btn == 2)  navigator.notification.beep(1)
+                                                    },"tabla reserva","Vibrar,Sonar,Cancelar ");
+                }
+        });
+    }
 }
